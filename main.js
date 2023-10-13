64a2e82815c24c197cac78548ddd29ce4ba3a2af
Anais Laguna <annslaguna@gmail.com>
2023-10-12 22:10:23 -0600

    consol.log

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log ("Hola")
+
+let a = 12;
+let b = 24;
+let result1 = (a+b)
+console.log (result1)
+
+a = 28;
+b = 365;
+let result2 = (a*b)
+console.log (result2)
+
+a = 14;
+b = 28;
+let result3 = (a/b)
+console.log (result3)
+
+a = 368;
+b = 512;
+let result4 = (a-b)
+console.log (result4)
+
+
+
+
+
+    
+
+
+
+
+
+
